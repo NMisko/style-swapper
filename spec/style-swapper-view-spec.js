4a981312f6e594ea41e917f60d0275f00f72ddfa
Nicola Miskowiec <nicola.miskowiec@gmail.com>
2016-12-23 12:19:44 +0100

    First commit

---
+++ spec/style-swapper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StyleSwapperView from '../lib/style-swapper-view';
+
+describe('StyleSwapperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
